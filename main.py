bde5397b8e190a53ad5a25582d63db6fb679fd4d
Diana <102837758+dvnoriega@users.noreply.github.com>
2025-05-30 16:58:55 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,32 +6,50 @@ import os
 model = Model("Reutilizacion_Aguas_Grises")
 
 # --- Conjuntos (Ejemplo inicial) ---
-B = ['b1', 'b2']  # Edificios
-S = ['s1']        # Sistemas de tratamiento
-V = ['v1']        # Estanques
-NG = ['ng1']      # Nodos disposición aguas grises
-NT = ['nt1']      # Nodos disposición aguas tratadas
-DC = ['dc']       # Eliminación por mala calidad
-DV = ['dv']       # Eliminación por tiempo de almacenamiento
+B = pd.read_csv('B.csv')['b'].tolist()  # Edificios
+S = pd.read_csv('S.csv')['s'].tolist()  # Sistemas de tratamiento
+V = pd.read_csv('V.csv')['v'].tolist()  # Estanques
+NG = pd.read_csv('NG.csv')['ng'].tolist()   # Nodos disposición aguas grises
+NT = pd.read_csv('NT.csv')['nt'].tolist()   # Nodos disposición aguas tratadas
+DC = pd.read_csv('DC.csv')['dc'].tolist()   # Eliminación por mala calidad
+DV = pd.read_csv('DV.csv')['dv'].tolist()   # Eliminación por tiempo de almacenamiento
 T = range(30)     # Días del mes
 
 N = B + S + V + NG + NT + DC + DV
 A = [(i, j) for i in N for j in N if i != j]
 
 # --- Parámetros (ejemplo simplificado) ---
-gamma = {'s1': 42084}
-kappa = {v: 1000 for v in V}
-#omega = 0.7
-mu = {('s1', 'nt1', t): 0.8 for t in T}  # calidad
-phi = {t: 100 for t in T}
-delta = {t: 50 for t in T}
-C = {(i, j): 1000 for (i, j) in A}
-T_sigma = {(i, j): 1 for (i, j) in A}
-S_sigma = {s: 3 for s in S}
-L_sigma = {s: 436099 for s in S}
-L_sigma.update({v: 0 for v in V}) 
-O = {(b, t): 500 for b in B for t in T}
-P = {(b, t): 400 for b in B for t in T}
+gamma_df = pd.read_csv('gamma.csv')
+gamma = dict(zip(gamma_df['s'], gamma_df['gamma']))
+
+kappa_df = pd.read_csv('kappa.csv')
+kappa = dict(zip(kappa_df['v'], kappa_df['kappa']))
+
+phi_df = pd.read_csv('phi.csv')
+phi = dict(zip(phi_df['t'], phi_df['phi']))
+
+delta_df = pd.read_csv('delta.csv')
+delta = dict(zip(delta_df['t'], delta_df['delta']))
+
+C_df = pd.read_csv('C.csv')
+C = {(row.i, row.j): row.C for row in C_df.itertuples()}
+
+T_sigma_df = pd.read_csv('T_sigma.csv')
+T_sigma = {(row.i, row.j): row.T_sigma for row in T_sigma_df.itertuples()}
+
+
+S_sigma_df = pd.read_csv('S_sigma.csv')
+S_sigma = dict(zip(S_sigma_df['s'], S_sigma_df['S_sigma']))
+
+L_sigma_df = pd.read_csv('L_sigma.csv')
+L_sigma = dict(zip(L_sigma_df['i'], L_sigma_df['L_sigma']))
+
+O_df = pd.read_csv('O.csv')
+O = {(row.b, row.t): row.O for row in O_df.itertuples()}
+
+P_df = pd.read_csv('P.csv')
+P = {(row.b, row.t): row.P for row in P_df.itertuples()}
+
 
 # --- Variables de decisión (inicial) ---
 AG = model.addVars(A, T, lb=0, name="AG")  # Agua gris
@@ -67,7 +85,7 @@ model.addConstrs((z[v,t] >= Bv[v,t]/kappa[v] for v in V for t in T), name = "R4"
 # R5: Cada 48 horas se vacía el estanque. 
 for v in V:
     for t in range(len(T) - 2):
-        model.addConstr(y[v, t] + y[v, t+1] + y[v, t+2] >= z[v, t])
+        model.addConstr(y[v, t] + y[v, t+1] + y[v, t+2] >= z[v, t], name = "R5")
 
 # R6: El volumen almacenado no supera la cota máxima, y si está vacío no hay agua en el estanque.  
 for v in V:
@@ -76,18 +94,18 @@ for v in V:
         model.addConstr(z[v, t] >= y[v, t])
         model.addConstr(z[v, t] >= Bv[v, t] / kappa[v], name = "R6")
 
-#R7: Verificar que el agua cumpla con la calidad mínima.  
-for (s, j, t) in mu:
-    model.addConstr(e[s, j, t] * omega <= mu[s, j, t])
-    model.addConstr(AT[s, j, t] <= C[s, j] * e[s, j, t], name = "R7")
+#R7: Asegurar que solo fluya agua tratada si se cumple calidad
+model.addConstrs(
+    (AT[s, j, t] <= C[s, j] * phi[s, j, t] 
+     for s in S for j in NT for t in T),
+    name="R7"
+)
 
-# R8:Asegurar que solo fluya agua tratada si se cumple calidad
-model.addConstrs((AT[s,j,t] <= C[s,j]*e[s,j,t] for j in NT for s in S if (s,j) in A for t in T), name = "R8") 
+# R8: Se cumple con la demanda del bloque b. 
+model.addConstrs((quicksum(AT[i, b, t] for i in N if (i, b) in A) >= P[b, t] for b in B for t in T), name="R8")
 
-# R9: Se cumple con la demanda del bloque b. 
-model.addConstrs((quicksum(AT[i, b, t] for i in N if (i, b) in A) >= P[b, t] for b in B for t in T), name="R9")
 
-# R10: La cantidad de agua gris que entra al sistema de trata es la misma que la que sale. 
+# R9: La cantidad de agua gris que entra al sistema de trata es la misma que la que sale. 
 for s in S:
     for t in T:
         model.addConstr(
@@ -97,43 +115,43 @@ for s in S:
         model.addConstr(
             quicksum(AG[i, s, t] for (i, j) in A if j == s) +
             quicksum(AT[s, j, t] for (i, j) in A if i == s) <= 2 * gamma[s] * (1 - m[s, t]),
-            name = "R10"
+            name = "R9"
         ) 
 
-#R12: Al almacén de agua tratada no entran aguas grises
+#R10: Al almacén de agua tratada no entran aguas grises
 model.addConstrs((
     quicksum(AG[i, v, t] for i in N if (i, v) in A) +
     quicksum(AG[v, j, t] for j in N if (v, j) in A) == 0
     for v in V for t in T
-), name="R12")
+), name="R10")
 
-#R12: Alalmacén de agua tratada no entran aguas grises
-model.addConstrs((quicksum(AG[i,v,t] for i in N if (i,v) in A) + quicksum(AG[v,j,t] for j in N if (v,j) in A) == 0 for v in V for t in T),name = "R12")
+#R11: Al almacén de agua tratada no entran aguas grises
+model.addConstrs((quicksum(AG[i,v,t] for i in N if (i,v) in A) + quicksum(AG[v,j,t] for j in N if (v,j) in A) == 0 for v in V for t in T),name = "R11")
 
-#R13 
-model.addConstrs((quicksum(AT[v,j,t] for j in N if (v,j) in A) + quicksum(AT[i,v,t] for i in N if (i,v) in A) <=  kappa[v] * (1 - m[v,t]) for v in V for t in T), name = "R13") 
+#R12: Si el almacen de agua tratada esta en mantención no tiene flujo de agua
+model.addConstrs((quicksum(AT[v,j,t] for j in N if (v,j) in A) + quicksum(AT[i,v,t] for i in N if (i,v) in A) <=  kappa[v] * (1 - m[v,t]) for v in V for t in T), name = "R12") 
 
-#R14: Si cumplo con la calidad,no desecho el agua tratada.
+#R13: Si cumplo con la calidad,no desecho el agua tratada.
 model.addConstrs((
     AT[j, dc, t] <= C[j, dc] * (1 - e[s, j, t])
     for s in S for j in NT for dc in DC for t in T
     if (j, dc) in A and (s, j, t) in e
-), name="R14")
+), name="R13")
 
-#R15: Si no se vacía el alamcén, no se elimina el agua tratada.
+#R14: Si no se vacía el alamcén, no se elimina el agua tratada.
 model.addConstrs((
     AT[v, dv, t] <= kappa[v] * y[v, t]
     for v in V for dv in DV if (v, dv) in A for t in T
-), name="R15")
+), name="R14")
 
-#R16: Definicion de Ht.
+#R15: Definicion de Ht.
 model.addConstrs((
     Ht[t] == quicksum(AT[i, dc, t] for i in N for dc in DC if (i, dc) in A) +
              quicksum(AT[i, dv, t] for i in N for dv in DV if (i, dv) in A)
     for t in T
-), name="R16")
+), name="R15")
 
-# R17: Balance de aguas grises.
+# R16: Balance de aguas grises.
 model.addConstrs((
     quicksum(AG[i, ng, t] for i in N if (i, ng) in A) -
     quicksum(AG[ng, j, t] for j in N if (ng, j) in A) ==
@@ -141,34 +159,34 @@ model.addConstrs((
     Di[ng, t] -
     quicksum(AG[i, s, t] for i in N for s in S if (i, s) in A)
     for ng in NG for t in T
-), name="R17")
+), name="R16")
 
 
-#R18: Balance de aguas tratadas en toda la red.
+#R17: Balance de aguas tratadas en toda la red.
 model.addConstrs((
     quicksum(AT[j, n, t] for j in N for n in NT + V if (j, n) in A) -
     quicksum(AT[n, j, t] for n in NT + V for j in N if (n, j) in A) -
     quicksum(AT[j, b, t] for j in N for b in B if (j, b) in A) ==
     quicksum(AT[i, s, t] for i in N for s in S if (i, s) in A) - Ht[t]
     for t in T
-), name="R18")
+), name="R17")
 
 
-#R19: Se hace mínimo una mantención mensual al sistema.
-model.addConstrs((quicksum(m[s, t] for t in T) >= 1 for s in S), name="R19")
+#R18: Se hace mínimo una mantención mensual al sistema.
+model.addConstrs((quicksum(m[s, t] for t in T) >= 1 for s in S), name="R18")
 
-#R20: No superar el caudal máximo de los arcos.
+#R19: No superar el caudal máximo de los arcos.
 model.addConstrs((
     AG[i, j, t] + AT[i, j, t] <= C[i, j] * x[i, j, t]
     for (i, j) in A for t in T
-), name="R20")
+), name="R19")
 
-#R21: Balance de nodos intermedios.
+#R20: Balance de nodos intermedios.
 model.addConstrs((
     quicksum(AG[i, n, t] + AT[i, n, t] for i in N if (i, n) in A) ==
     quicksum(AG[n, j, t] + AT[n, j, t] for j in N if (n, j) in A)
     for n in NG + NT for t in T
-), name="R21")
+), name="R20")
 
 # --- Función Objetivo ---
 model.setObjective(
